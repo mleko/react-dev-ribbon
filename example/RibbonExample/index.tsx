708567499ad69ea8ba6fad97119f59f1841c773e
Daniel Król <daniel@krol.me>
2017-02-27 21:39:50 +0100

    Adjustable ribbon width, angle

--- example/RibbonExample/index.tsx
+++ example/RibbonExample/index.tsx
@@ -6,7 +6,8 @@ export class RibbonExample extends React.PureComponent<void, State> {
 	public constructor(props) {
 		super(props);
 		this.state = {
-			text: "DEVELOPMENT"
+			text: "DEVELOPMENT",
+			width: ""
 		};
 	}
 
@@ -16,7 +17,10 @@ export class RibbonExample extends React.PureComponent<void, State> {
 				<label>Ribbon text:
 					<input value={this.state.text} onChange={this.changeText}/>
 				</label>
-				<Ribbon>{this.state.text}</Ribbon>
+				<label>Ribbon width:
+					<input type="number" value={this.state.width} onChange={this.changeWidth}/>
+				</label>
+				<Ribbon width={+this.state.width} angle={45}>{this.state.text}</Ribbon>
 			</div>
 		);
 	}
@@ -24,8 +28,12 @@ export class RibbonExample extends React.PureComponent<void, State> {
 	private changeText = (event: React.FormEvent<HTMLInputElement>) => {
 		this.setState({text: event.currentTarget.value});
 	};
+	private changeWidth = (event: React.FormEvent<HTMLInputElement>) => {
+		this.setState({width: event.currentTarget.value});
+	};
 }
 
 interface State {
 	text?: string;
+	width?: string;
 }
--- src/Ribbon/index.tsx
+++ src/Ribbon/index.tsx
@@ -1,19 +1,30 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 export class Ribbon extends React.PureComponent<RibbonProps, void> {
+	private container: HTMLDivElement;
+
 	public render(): JSX.Element {
+		const width = this.props.width || 190;
+		const angle = this.props.angle || 45;
+		const sin = Math.sin(Math.PI / 180 * angle);
+		const cos = Math.cos(Math.PI / 180 * angle);
+
+		let height = (this.container && this.container.clientHeight) || 0;
+		let top = Math.floor(width / 2 * sin - cos * height);
+		let right = Math.floor(-(width / 2 - width / 2 * cos) - sin * height / 2);
 		const ribbonStyle: React.CSSProperties = {
 			fontFamily: "monospace",
 			fontSize: "16px",
 			textAlign: "center",
-			width: 190,
+			width,
 			color: "white",
 			backgroundColor: "#c00",
-			transform: "rotate(45deg)",
+			transform: `rotate(${angle}deg)`,
 			zIndex: 9999,
 			position: "fixed",
-			top: 30,
-			right: -50,
+			top, // 30,
+			right, // -50
 			fontWeight: "bold",
 			padding: 5,
 			cursor: "pointer",
@@ -23,13 +34,19 @@ export class Ribbon extends React.PureComponent<RibbonProps, void> {
 			border: "1px white dashed"
 		};
 		return (
-			<div style={ribbonStyle}>
+			<div style={ribbonStyle} ref={this.mountContainer}>
 				<div style={ribbonTextStyle}>{this.props.children || "DEVELOPMENT"}</div>
 			</div>
 		);
 	}
+
+	private mountContainer = (e: HTMLDivElement) => {
+		this.container = ReactDOM.findDOMNode<HTMLDivElement>(e);
+		this.forceUpdate();
+	}
 }
 // onclick="var s=this.style,p='opacity',t=setTimeout,i=function(){if(s[p]<1){s[p]-=-0.05;s.display='block';t(i,10);}else{s[p]=1;}},o=function(){if(s[p]>0){s[p]-=0.05;t(o,10);}else{s[p]=0;s.display='none';t(i,5e+3);}};o();"
 export interface RibbonProps {
-
+	width?: number;
+	angle?: number;
 }
