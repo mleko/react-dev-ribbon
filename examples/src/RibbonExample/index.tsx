0facf3145aca30d57d26069bd724ae1edf388616
Daniel Król <daniel@krol.me>
2017-04-06 07:39:53 +0200

    Add more examples

--- examples/src/RibbonExample/index.tsx
+++ examples/src/RibbonExample/index.tsx
@@ -1,5 +1,15 @@
 import * as React from "react";
-import {DismissibleRibbon as Ribbon} from "../../../";
+import {mergeDeep} from "typescript-object-utils";
+import {DismissibleRibbon, Ribbon} from "../../../";
+
+const examples = [
+	{},
+	{style: {background: "green"}},
+	{style: {background: "blue"}},
+	{style: {background: "black"}},
+	{style: {color: "black", background: "#ccc"}},
+	{children: "STAGE"},
+];
 
 export class RibbonExample extends React.PureComponent<void, State> {
 
@@ -14,15 +24,36 @@ export class RibbonExample extends React.PureComponent<void, State> {
 	public render(): JSX.Element {
 		return (
 			<div>
-				<label>Ribbon text:
-					<input value={this.state.text} onChange={this.changeText}/>
-				</label>
-				<label>Ribbon width:
-					<input type="number" value={this.state.width} onChange={this.changeWidth}/>
-				</label>
-				<Ribbon width={+this.state.width}>{this.state.text}</Ribbon>
+				<div style={{height: 200}}>
+					<label>Ribbon text:
+						<input value={this.state.text} onChange={this.changeText}/>
+					</label>
+					<label>Ribbon width:
+						<input type="number" value={this.state.width} onChange={this.changeWidth}/>
+					</label>
+					<DismissibleRibbon width={+this.state.width}>{this.state.text}</DismissibleRibbon>
+				</div>
+				<div>{this.renderExamples()}</div>
 			</div>
+
 		);
+	}
+
+	public renderExamples() {
+		const cStyle = {
+			height: 200,
+			width: 200,
+			background: "#f5f5f5",
+			margin: 5,
+			overflow: "hidden",
+			display: "inline-block"
+		};
+		return examples.map((example, i) => {
+			let props = mergeDeep(example, {style: {position: "relative"}});
+			return (<div style={cStyle} key={"example-" + i}>
+				{React.createElement(Ribbon, props)}
+			</div>);
+		});
 	}
 
 	private changeText = (event: React.FormEvent<HTMLInputElement>) => {
